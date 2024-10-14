bb70d23485b3cca473f28d3c201c067507dce3c9
BroGames <hunempire11@gmail.com>
2024-10-14 12:25:59 +0200

    save

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Post, Body, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express'
 
 @Controller()
 export class AppController {
@@ -11,5 +12,39 @@ export class AppController {
     return {
       message: this.appService.getHello()
     };
+  }
+
+  @Get('Foglalas')
+  @Render('Foglalas')
+  foglalas() {
+    return {
+      data: [],
+      errors: []
+    }
+  }
+
+  @Post('Foglalas')
+  postFoglalas(@Body() foglalasDto: FoglalasDto,@Res() response: Response) {
+    var errors = []
+    var data = foglalasDto
+    console.log(foglalasDto);
+    if(!foglalasDto.nev || !foglalasDto.email || !foglalasDto.date || !foglalasDto.viewers){
+      errors.push("Minden adatot meg kell adni!")
+    }
+    console.log(errors.length)
+    if(errors.length > 0){
+      response.render('foglalas')
+      return {
+        data: data,
+        errors: errors
+      }
+      
+    }
+    response.redirect("siker")
+  }
+
+  @Get('siker')
+  @Render('siker')
+  siker(){
   }
 }
